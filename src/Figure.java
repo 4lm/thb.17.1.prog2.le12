12d82063bed6cd31a3f4a0f90d44e13fcf464257
Alexis Michaltsis <alexis@michaltsis.net>
2017-05-04 17:24:34 +0200

    Add Figure class

---
+++ src/Figure.java
@@ -0,0 +1,65 @@
+abstract class Figure {
+    protected final double PI = Math.PI;
+    protected double x, y, a, b;
+    protected Color fillColor;
+    protected Color lineColor;
+    Figure() {
+	x = 0;
+	y = 0;
+        a = 0;
+	b = 0;
+	fillColor.setRGB(0,0,0);
+	lineColor.setRGB(0,0,0);
+    }
+    Figure(double x, double y, double a, double b) {
+	this.x = x;
+	this.y = y;
+        this.a = a;
+	this.b = b;
+	fillColor.setRGB(0,0,0);
+	lineColor.setRGB(0,0,0);
+    }
+    Figure(double x, double y, double a, double b, Color fC, Color lC) {
+	this.x = x;
+	this.y = y;
+        this.a = a;
+	this.b = b;
+	fillColor = fC;
+	lineColor = lC;
+    }
+    public void setPos(double x, double y) {
+	this.x = x;
+	this.y = y;
+    }
+    public void setDim(double a, double b) {
+	this.a = a;
+	this.b = b;
+    }
+    public void setColors(Color fC, Color lC) {
+	fillColor = fC;
+	lineColor = lC;
+    }
+    public String getForm() {
+	return "Figure";
+    }
+    public double getX() {
+	return x;
+    }
+    public double getY() {
+	return y;
+    }
+    public double getWidth() {
+	return a;
+    }
+    public double getHeight() {
+	return b;
+    }
+    public String getColors() {
+	return "Fill: " + fillColor.getRGB() + " Line: " + lineColor.getRGB();
+    }
+    public String toString() {
+	return getForm();
+    }
+    public abstract double circumference();
+    public abstract double area();
+}
