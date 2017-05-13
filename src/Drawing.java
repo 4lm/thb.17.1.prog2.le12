bac525585f719adef71502a40d929f3ad805211e
Alexis Michaltsis <alexis@michaltsis.net>
2017-05-13 14:50:52 +0200

    Add Drawing class

---
+++ src/Drawing.java
@@ -0,0 +1,26 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class Drawing {
+    private List<Figure> shapes = new ArrayList<Figure>();
+    private int indexCounter = -1;
+    
+    public void addShape(Figure f) {
+	shapes.add(f);
+	indexCounter++;
+    }
+    public void removeShape(int i) {
+	shapes.remove(i);
+	indexCounter--;
+    }
+    public int getIndexCounter() {
+	return indexCounter;
+    }
+    public void printShape(int i) {
+	System.out.println("Type         : " + shapes.get(i).toString());
+	System.out.println("Width        : " + shapes.get(i).getWidth());
+	System.out.println("Height       : " + shapes.get(i).getHeight());
+	System.out.println("Circumference: " + shapes.get(i).circumference());
+	System.out.println("Area         : " + shapes.get(i).area());
+    }
+}
