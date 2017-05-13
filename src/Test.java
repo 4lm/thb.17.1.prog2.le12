dc314f9a1865cdb4883a423cc8b67634db1082b0
Alexis Michaltsis <alexis@michaltsis.net>
2017-05-13 16:27:21 +0200

    Update Test class

--- src/Test.java
+++ src/Test.java
@@ -13,53 +13,27 @@ public class Test {
 	else
 	    System.out.println("Color t2: fail - expected: 16711935; delivered: " + color.getRGB());
 	// Test Figures
-	Figure circle = new Circle(0,0,10,10);
-	Figure ellipse = new Ellipse(0,0,10,5);
-	Figure square = new Square(0,0,10,10);
-	Figure rectangle = new Rectangle(0,0,10,5);
-	Figure line = new Line(0,0,1,10);
-	Figure triangle = new Triangle(0,0,10,10);
-	Figure polygon = new Polygon(0,0,8,0.765366865);
-	System.out.println("\nCircle       : " + circle.toString());
-	System.out.println("Width        : " + circle.getWidth());
-	System.out.println("Height       : " + circle.getHeight());
-	System.out.println("Circumference: " + circle.circumference());
-	System.out.println("Area         : " + circle.area());
-	
-	System.out.println("\nEllipse      : " + ellipse.toString());
-	System.out.println("Width        : " + ellipse.getWidth());
-	System.out.println("Height       : " + ellipse.getHeight());
-	System.out.println("Circumference: " + ellipse.circumference());
-	System.out.println("Area         : " + ellipse.area());
-	
-	System.out.println("\nSquare       : " + square.toString());
-	System.out.println("Width        : " + square.getWidth());
-	System.out.println("Height       : " + square.getHeight());
-	System.out.println("Circumference: " + square.circumference());
-	System.out.println("Area         : " + square.area());
-	
-	System.out.println("\nRectangle    : " + rectangle.toString());
-	System.out.println("Width        : " + rectangle.getWidth());
-	System.out.println("Height       : " + rectangle.getHeight());
-	System.out.println("Circumference: " + rectangle.circumference());
-	System.out.println("Area         : " + rectangle.area());
-	
-	System.out.println("\nLine         : " + line.toString());
-	System.out.println("Width        : " + line.getWidth());
-	System.out.println("Height       : " + line.getHeight());
-	System.out.println("Circumference: " + line.circumference());
-	System.out.println("Area         : " + line.area());
-	
-	System.out.println("\nTriangle     : " + triangle.toString());
-	System.out.println("Width        : " + triangle.getWidth());
-	System.out.println("Height       : " + triangle.getHeight());
-	System.out.println("Circumference: " + triangle.circumference());
-	System.out.println("Area         : " + triangle.area());
-	
-        System.out.println("\nPolygon      : " + polygon.toString());
-	System.out.println("Width        : " + polygon.getWidth());
-	System.out.println("Height       : " + polygon.getHeight());
-	System.out.println("Circumference: " + polygon.circumference());
-	System.out.println("Area         : " + polygon.area());
+	Drawing draw = new Drawing();
+	draw.addShape(new Circle(1,2,10,10));
+	draw.addShape(new Ellipse(2,3,10,5));
+	draw.addShape(new Square(4,5,10,10));
+	draw.addShape(new Rectangle(6,7,10,5));
+	draw.addShape(new Line(8,9,1,10));
+	draw.addShape(new Triangle(8,7, 10,10));
+	draw.addShape(new Polygon(10,10,2,2,3));
+	draw.addShape(new Polygon(10,10,2,2,4));
+	draw.addShape(new Polygon(10,10,2,2,5));
+        draw.addShape(new Polygon(10,10,2,2,6));
+	draw.addShape(new Polygon(10,10,2,2,8));
+	draw.addShape(new Polygon(10,10,2,2,10));
+	draw.addShape(new Polygon(10,10,2,2,12));
+	draw.addShape(new Polygon(10,10,2,2,100));
+	draw.addShape(new Polygon(10,10,2,2,1000));
+	draw.addShape(new Polygon(10,10,2,2,10000));
+	draw.addShape(new Polygon(10,10,2,2,100000));
+        for (int i = 0; i <= draw.getIndexCounter(); i++) {
+	    draw.printShape(i);
+	    System.out.println();
+	}
     }
 }
